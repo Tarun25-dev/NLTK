bd7958d3ec4b414bc87d0d1490523f5497e35eaf
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-16 21:40:54 +0530

    Add files via upload

---
+++ Day3/tokenization.py
@@ -0,0 +1,3 @@
+# Lemmatization - Smart root conversion similar to stemming only difference is It produces proper dictionary words.
+# NLTK needs external datasets to work.
+# so thats why we need to download and use these libraries 'wordnet' and 'omw-1.4'
